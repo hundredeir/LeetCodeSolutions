939d11dde2e37d3d0c2b4fec370bceb0a24da70c
hundredeir <20334754+hundredeir@users.noreply.github.com>
2020-11-08 19:55:28 +0530

    Add solution for all paths from source to target

---
+++ all_paths_from_source_to_target.py
@@ -0,0 +1,23 @@
+"""
+https://leetcode.com/problems/all-paths-from-source-to-target/
+"""
+from typing import List
+
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        last_node = len(graph) - 1
+        paths = []
+        self.rec_dfs(graph, 0, [0, ], paths, last_node)
+        return paths
+    
+    def rec_dfs(self, graph, node, path, paths, last_node):
+        children = graph[node]
+        if children:
+            for child in children:
+                if child == last_node:
+                    paths.append([*path, child])
+                else:
+                    path.append(child)
+                    self.rec_dfs(graph, child, path, paths, last_node)
+                    path.pop()
